b67b8d15f8c8e5f807d057b1cd2c512cb7c5cc90
hardit826 <hardit826@gmail.com>
2015-09-08 12:51:17 -0600

    ParticleCollision
    ParticleCollision

---
+++ Assets/Scripts/CollisionParticle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionParticle : MonoBehaviour
+{
+
+	public ParticleSystem particleEffect;
+
+	void OnControllerColliderHit(ControllerColliderHit hit) 
+	{
+		if(hit.gameObject.tag == "Wall")
+		{
+			Debug.Log("Bump !");
+			Instantiate(particleEffect,transform.position,Quaternion.identity);
+		}
+	}
+}
